c4deb87d15177e221b416db84c6c40e9bececa60
Allan Gaudio <allan.gaudio@kumulus.com>
2023-06-01 14:16:41 -0300

    fix $gte to $eq

--- src/routes/alerta_antibioticoprofilaxia-route.js
+++ src/routes/alerta_antibioticoprofilaxia-route.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/alerta_antibioticoprofilaxia-controller');
 
-router.get('/', controller.get);
+//router.get('/', controller.get);
 // router.get('/:id', controller.getById);
 router.get('/:partogramaId', controller.getByPartogramaId);
 router.post('/', controller.post);
